42d8eb25efe68fe33f8627edd96f8b32f893fe3b
NICOLA-200 <fatepepe66@gmail.com>
2023-05-11 21:55:37 +0200

    added javascript

---
+++ script.js
@@ -0,0 +1,9 @@
+let move_speed = 3 , gravity = 0.5
+
+let bird = document.querySelector (".bird")
+let img = document.getElementById("bird-1")
+
+
+let bird_props = bird.getBoundingClientRect()
+
+let background = document.querySelector(".background")
