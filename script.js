4ebdadade290a5f5076a4b80da89e77dfc00f763
NICOLA-200 <fatepepe66@gmail.com>
2023-05-12 08:47:44 +0200

    added some javascript

--- script.js
+++ script.js
@@ -1,9 +1,23 @@
-let move_speed = 3 , gravity = 0.5
+let move_speed = 3, grativy = 0.5;
+let bird = document.querySelector('.bird');
+let img = document.getElementById('bird-1');
+let sound_point = new Audio('sounds effect/point.mp3');
+let sound_die = new Audio('sounds effect/die.mp3');
 
-let bird = document.querySelector (".bird")
-let img = document.getElementById("bird-1")
+let background = document.querySelector(".background").getBoundingClientRect()
 
+let score_val = document.querySelector(".score_val");
+let message = document.querySelector(".message");
 
-let bird_props = bird.getBoundingClientRect()
+let score_title = document.querySelector(".score_title")
 
-let background = document.querySelector(".background")
+let game_state    =  "Start"
+
+img.style.display =  "none"
+message.classList.add("messageStyle")
+
+document.addEventListener("keydown", (e)=> {
+     if (e.key == "Enter && game_state != "Play") {
+          document.querySelectorAll(".pipe_sprite").
+     }
+})
