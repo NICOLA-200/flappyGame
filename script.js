ca6e1e792157f1833fb97a3db9f46dceefdc29ce
NICOLA-200 <fatepepe66@gmail.com>
2023-05-13 08:06:32 +0200

    added the entering

--- script.js
+++ script.js
@@ -14,10 +14,16 @@ let score_val = document.querySelector('.score_val');
 let message = document.querySelector('.message');
 let score_title = document.querySelector('.score_title');
 
-let game_state    =  "Start"
+let game_state = 'Start';
+img.style.display = 'none';
+message.classList.add('messageStyle');
 
-img.style.display =  "none"
-message.classList.add("messageStyle")
+document.addEventListener('keydown', (e) => {
+    
+    if(e.key == 'Enter' && game_state != 'Play'){
+        document.querySelectorAll('.pipe_sprite').forEach((e) => {
+            e.remove();
+        });
 
 document.addEventListener("keydown", (e)=> {
      if (e.key == "Enter && game_state != "Play") {
