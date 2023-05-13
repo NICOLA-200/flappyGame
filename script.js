408479c3d7985d8022318dc3399acdfccb80af99
NICOLA-200 <fatepepe66@gmail.com>
2023-05-13 08:05:44 +0200

    added some javascript

--- script.js
+++ script.js
@@ -4,12 +4,15 @@ let img = document.getElementById('bird-1');
 let sound_point = new Audio('sounds effect/point.mp3');
 let sound_die = new Audio('sounds effect/die.mp3');
 
-let background = document.querySelector(".background").getBoundingClientRect()
+// getting bird element properties
+let bird_props = bird.getBoundingClientRect();
 
-let score_val = document.querySelector(".score_val");
-let message = document.querySelector(".message");
+// This method returns DOMReact -> top, right, bottom, left, x, y, width and height
+let background = document.querySelector('.background').getBoundingClientRect();
 
-let score_title = document.querySelector(".score_title")
+let score_val = document.querySelector('.score_val');
+let message = document.querySelector('.message');
+let score_title = document.querySelector('.score_title');
 
 let game_state    =  "Start"
 
